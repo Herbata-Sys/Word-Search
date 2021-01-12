077c33c41d1c4f33e47baef53f62cf8392150002
Damian Dyas <damiandyas@vp.pl>
2021-01-12 20:26:53 +0100

    Fix service worker registration

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pwa: {
-    workboxPluginMode: 'GenerateSW',
+    workboxPluginMode: 'InjectManifest',
     themeColor: '#FFFFFF',
     manifestOptions: {
       icons: [
@@ -8,6 +8,9 @@ module.exports = {
         { "src":"./img/icons/android-chrome-512x512.png","sizes":"512x512","type":"image/png" },
         { "src":"./img/icons/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable" }
       ]
+    },
+    workboxOptions: {
+      swSrc: 'src/service-worker.js'
     }
   }
 }
