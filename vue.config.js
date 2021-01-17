05c9d745d19d5015a650b34a300b4065207e6a2e
Damian Dyas <damiandyas@vp.pl>
2021-01-17 20:30:39 +0100

    Add new icon, fix icon issue, change application name and colour

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,14 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     themeColor: '#FFFFFF',
     manifestOptions: {
+      name: "Wykreślanka",
+      short_name: "Wykreślanka",
       icons: [
         { "src":"./img/icons/android-chrome-192x192.png","sizes":"192x192","type":"image/png" },
         { "src":"./img/icons/android-chrome-512x512.png","sizes":"512x512","type":"image/png" },
         { "src":"./img/icons/android-chrome-maskable-192x192.png","sizes":"192x192","type":"image/png","purpose":"maskable" }
-      ]
+      ],
+      background_color: "#FFFFFF"
     },
     workboxOptions: {
       swSrc: 'src/service-worker.js'
