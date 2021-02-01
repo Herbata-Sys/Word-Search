a7c4deaf588f1aebd6a0bb2cb1d1c6943bfc7af5
Damian Dyas <damiandyas@vp.pl>
2021-02-01 01:39:00 +0100

    Fix sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,8 +3,9 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     updated (registration) {
-      const worker = registration.waiting
-      worker.postMessage({ action: 'SKIP_WAITING' })
+      window.location.reload(true)
+      // const worker = registration.waiting
+      // worker.postMessage({ action: 'SKIP_WAITING' })
     }
   })
 }
