0afe183ea367cc64d7033aca352d7cbd9d349970
Damian Dyas <damiandyas@vp.pl>
2021-02-01 01:17:08 +0100

    Add service workerk clear old cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,5 +10,17 @@ self.addEventListener('message', (event) => {
   }
 });
 
+self.addEventListener('activate', event => {
+  var cacheKeeplist = [cacheName];
+  event.waitUntil(
+    caches.keys().then( keyList => {
+        return Promise.all(keyList.map( key => {
+            if (cacheKeeplist.indexOf(key) === -1) {
+                return caches.delete(key);
+            }
+        }));
+    }).then(self.clients.claim()));
+});
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
