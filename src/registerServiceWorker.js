ae52aff78151f4384918d00705e0afb2d670a74b
Damian Dyas <damiandyas@vp.pl>
2021-02-01 02:13:38 +0100

    Add sw update message

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -3,9 +3,15 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     updated (registration) {
-      window.location.reload(true)
+      // window.location.reload(true)
       // const worker = registration.waiting
       // worker.postMessage({ action: 'SKIP_WAITING' })
+      if (registration.waiting) {
+        const accept = confirm("Nowa wersja jest dostępna, zainstalować?")
+        if (accept) {
+          registration.waiting.postMessage('SKIP_WAITING');
+        }
+      }
     }
   })
 }
